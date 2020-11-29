d569b2d8290f7894ff60fda2a95dd7428fa6f23a
Tony Benoy <me@tonybenoy.com>
2020-11-29 19:16:27 +0530

    font awesome

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import me from "./me.jpg";
 import "./App.css";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedinIn,
+  faTwitter,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={me} className="App-logo" alt="me" />
+        <img src={me} className="App-logo" alt="tonybenoy" />
         <h1>Tony Benoy</h1>
         <p>
           A Pretentious Noob
-          <span class="blink_text">_</span>
+          <span className="blink_text">_</span>
         </p>
+        <div>
+          <a href="https://www.linkedin.com/in/tonybenoy/">
+            <FontAwesomeIcon className="fa" icon={faLinkedinIn} />
+          </a>
+          <a href="https://twitter.com/TonyBenoy">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="https://github.com/tonybenoy">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://instagram.com/tonybenoy">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+        </div>
       </header>
     </div>
   );
 }
-
-// import me_image from "./me.jpg";
-// import "./tailwind.generated.css";
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <img class="img-circle" src={me_image} alt="tonybenoy" />
-//       <h1 className="text-yellow-400">Tony Benoy</h1>
-//       <p>
-//         A Pretentious Noob <span class="blink_text">_</span>
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default App;
 
 export default App;
---
+++ src/fontawesome.js
@@ -0,0 +1,10 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+import {
+  faLinkedinIn,
+  faTwitter,
+  faInstagram,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+library.add(faLinkedinIn, faTwitter, faInstagram, faGithub);
